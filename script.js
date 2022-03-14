abf908d3239967067e82fcf1c776d37b272a535f
Marcin <alpinus1@gmail.com>
2022-03-14 11:33:08 +0100

    few scss lines corrected

--- script.js
+++ script.js
@@ -3,19 +3,28 @@ const incomeSection = document.querySelector(`.income-area`);
 const expensesSection = document.querySelector(`.expenses-area`);
 const availableMoney = document.querySelector(`.available-money`);
 const addTransactionPanel = document.querySelector(`.add-transaction-panel`);
+
 // inputy
 const nameInput = document.querySelector(`#name`);
 const amountInput = document.querySelector(`#amount`);
 const categorySelect = document.querySelector(`#category`);
+
 // przyciski
-const addTransactionBtn = document.querySelector(`add-transaction`);
+const addTransactionBtn = document.querySelector(`.add-transaction`);
 const saveBtn = document.querySelector(`.save`);
 const cancelBtn = document.querySelector(`.cancel`);
 const deleteBtn = document.querySelector(`.delete`);
 const deleteAllBtn = document.querySelector(`.delete-all`);
+
 // rest
 let root = document.documentElement;
 let ID = 0;
 let categoryIcon;
 let selectedCategory;
 let moneyArr = [0];
+
+const showPanel = () => {
+	addTransactionPanel.style.display = `flex`;
+};
+
+addTransactionBtn.addEventListener(`click`, showPanel);
